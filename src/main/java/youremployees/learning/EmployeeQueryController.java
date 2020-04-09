5fdcfa738b33caa05e2e7b47bb65e51eb9ad919b
insalespacecat <60538056+insalespacecat@users.noreply.github.com>
2020-04-09 20:12:32 +0300

    Release Version

--- src/main/java/youremployees/learning/EmployeeQueryController.java
+++ src/main/java/youremployees/learning/EmployeeQueryController.java
@@ -42,10 +42,12 @@ public class EmployeeQueryController {
     @PatchMapping(path="/patch/{EmployeeId}", consumes="application/json")
     public ResponseEntity<Employee> patchEmployee(@PathVariable("EmployeeId") Long employeeId, @RequestBody Employee patch) {
         Optional<Employee> employeeOpt = employeeRepository.findById(employeeId);
+        log.info("patch request");
         if(employeeOpt.isEmpty()) {
             return new ResponseEntity<Employee>(patch, HttpStatus.NOT_FOUND);
         }
         Employee employeeToPatch = employeeOpt.get();
+        log.info(employeeToPatch.toString());
         if(patch.getName() != null){
             employeeToPatch.setName(patch.getName());
         }
@@ -59,12 +61,13 @@ public class EmployeeQueryController {
         if(patch.getWorkplace() != null){
             employeeToPatch.setWorkplace(patch.getWorkplace());
         }
-        employeeRepository.save(employeeToPatch);
-        return new ResponseEntity<Employee>(employeeToPatch, HttpStatus.ACCEPTED);
+        return new ResponseEntity<Employee>(employeeRepository.save(employeeToPatch), HttpStatus.ACCEPTED);
     }
     @DeleteMapping(path = "/delete/{employeeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteOrder(@PathVariable("employeeId") Long employeeId) {
+        log.info("Delete request!");
+        log.info(employeeId.toString());
         try {
             employeeRepository.deleteById(employeeId);
         } catch (EmptyResultDataAccessException ignored) {}
--- src/main/java/youremployees/learning/EmployeeRepository.java
+++ src/main/java/youremployees/learning/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package youremployees.learning;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
         Employee findByName(String name);
 }
